0eafba91946573f9e02377625fd05396c5728721
Steve Beckert <stevebeckert@hotmail.com>
2020-05-09 18:52:37 -0700

    Added Label and Button using BroderLayout

--- JavaGUIExample/jge.java
+++ JavaGUIExample/jge.java
@@ -1,5 +1,7 @@
 package JavaGUIExample;
 
+import java.awt.BorderLayout;
+
 import javax.swing.*;
 
 public class jge {
@@ -7,8 +9,11 @@ public class jge {
         JFrame frame = new JFrame("My First GUI");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
+        JLabel label = new JLabel("Hello, GUI World!");
+        label.setHorizontalAlignment(JLabel.CENTER);
         JButton button1 = new JButton("Press Me!");
-        frame.getContentPane().add(button1);
+        frame.getContentPane().add(BorderLayout.NORTH, label);
+        frame.getContentPane().add(BorderLayout.CENTER, button1);
         frame.setVisible(true);
     }
 }
