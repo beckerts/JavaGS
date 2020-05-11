e9dc0ab82cda508cd847ade2608a57d094e34da9
Steve Beckert <stevebeckert@hotmail.com>
2020-05-10 19:54:46 -0700

    My first netbeans maven project.

---
+++ JavaMaven/mavenproject1/src/main/java/net/piperpark/mavenproject1/Main.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.piperpark.mavenproject1;
+
+/**
+ *
+ * @author Steve
+ */
+public class Main {
+    public static void main(String[] args) {
+        OutputHelloWorldToSystemOut();
+        OutputHelloWorldAgainToSystemOut();
+    }
+    
+    private static void OutputHelloWorldToSystemOut()
+    {
+        System.out.println("Hello, Netbeans World!");
+    }
+    
+    private static void OutputHelloWorldAgainToSystemOut()
+    {
+        System.out.println("Hello, Netbeans World! Again");
+    }
+}
