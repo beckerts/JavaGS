e0f2d28ddcc1a88adaea2a3a99c034aeb1a74659
Steve Beckert <stevebeckert@hotmail.com>
2020-05-11 15:58:08 -0700

    Fixed maven 1.7 warning. Added a couple of println statements. Ran unit tests and they passed.

--- MavenQS/qs/src/main/java/piperpark/App.java
+++ MavenQS/qs/src/main/java/piperpark/App.java
@@ -8,6 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Maven World!" );
+        System.out.println( "Hello World! from San Mateo" );
+        System.out.println( "Hello Maven World! from Guerneville" );
     }
 }
