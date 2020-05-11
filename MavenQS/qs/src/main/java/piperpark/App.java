1ee37f3e5da4b638b041f9031ea975525b5f163b
Steve Beckert <stevebeckert@hotmail.com>
2020-05-11 15:51:13 -0700

    MavenQS Initial commit

---
+++ MavenQS/qs/src/main/java/piperpark/App.java
@@ -0,0 +1,13 @@
+package piperpark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
