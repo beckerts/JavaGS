1e06316e06a0f8864b48e42ccd5c8a0841b31226
Steve Beckert <stevebeckert@hotmail.com>
2020-05-13 11:03:22 -0700

    Maven JUnit5 in VS Code

---
+++ JavaMaven/MavenCodeUT/mavencodeut/src/main/java/net/piperpark/App.java
@@ -0,0 +1,13 @@
+package net.piperpark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
